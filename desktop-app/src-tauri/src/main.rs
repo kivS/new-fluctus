410710d5c392b47c42ffcc81d3901a0cbdd06d8d
Vik Borges <v1k@protonmail.com>
2022-01-16 22:12:33 +0000

    added tray icon.. it's something!

--- desktop-app/src-tauri/src/main.rs
+++ desktop-app/src-tauri/src/main.rs
@@ -3,8 +3,20 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::SystemTray;
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+
 fn main() {
+
+  let tray_menu = SystemTrayMenu::new()
+  .add_item(CustomMenuItem::new("about".to_string(), "About"))
+  .add_native_item(SystemTrayMenuItem::Separator)
+  .add_item(CustomMenuItem::new("quit".to_string(), "Quit"));
+
+  let tray = SystemTray::new().with_menu(tray_menu);
+
   tauri::Builder::default()
+    .system_tray(tray)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }

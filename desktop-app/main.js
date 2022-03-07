a7cfa348eec52959ab71e70f945d6318d1938b2a
Vik Borges <v1k@protonmail.com>
2022-03-07 22:31:58 +0000

    added auto update

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -1,6 +1,36 @@
-const { app, BrowserWindow, Menu, Tray, globalShortcut, dialog, shell } = require('electron')
+const { app, BrowserWindow, Menu, Tray, globalShortcut, dialog, shell, autoUpdater } = require('electron')
 const path = require('path')
 const { URL } = require('url')
+
+if(app.isPackaged) {
+    //
+    // Process auto updates
+    // 
+    const server = 'https://vercel.com/kivs/fluctus-update-server/GesxTNxtkStz3cJnr77S5HxzsZYM'
+    const url = `${server}/update/${process.platform}/${app.getVersion()}`
+    autoUpdater.setFeedURL({url})
+
+    autoUpdater.checkForUpdates() // check for updates every time the app is started
+
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+        const dialogOpts = {
+            type: 'info',
+            buttons: ['Restart', 'Later'],
+            title: 'Application Update',
+            message: process.platform === 'win32' ? releaseNotes : releaseName,
+            detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+        }
+
+        dialog.showMessageBox(dialogOpts).then((returnValue) => {
+            if (returnValue.response === 0) autoUpdater.quitAndInstall()
+        })
+    })
+
+    autoUpdater.on('error', message => {
+        console.error('There was a problem updating the application')
+        console.error(message)
+    })
+}
 
 if (process.defaultApp) {
     if (process.argv.length >= 2) {
